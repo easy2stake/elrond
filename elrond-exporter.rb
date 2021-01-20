99ecbcb858bb41d1dd356e9ebe862a681bae8e39
Alex <alex@easy2stake.com>
2021-01-20 22:57:46 +0200

    improved logic and more metrics

--- elrond-exporter.rb
+++ elrond-exporter.rb
@@ -46,9 +46,8 @@ def setMetaLabel(shardid)
 	end
 end
 
-def extract_info(heartbeats_array,statistics_hash,identity)
+def extract_info(heartbeats_array,statistics_hash)
 	heartbeats_array.each do |heartbeat|
-		# if heartbeat['identity'] == identity
 			metricLabels = "displayName=\"#{heartbeat['nodeDisplayName']}\",nodeType=\"#{heartbeat['peerType']}\",shardID=\"#{setMetaLabel(heartbeat['receivedShardID'])}\",validatorPubkey=\"#{heartbeat['publicKey']}\",identity=\"#{heartbeat['identity']}\",version=\"#{heartbeat['versionNumber']}\""
 			puts "elrond_node_r_is_active{#{metricLabels}} #{true_false(heartbeat['isActive'])}"
 			puts "elrond_node_r_total_uptime_sec{#{metricLabels}} #{heartbeat['totalUpTimeSec']}"
@@ -58,31 +57,71 @@ def extract_info(heartbeats_array,statistics_hash,identity)
 			if heartbeat['peerType'] != "observer"
 			 	extract_statistics(statistics_hash,"#{heartbeat['publicKey']}", metricLabels)
 		    end
-	    # end
 	end
 end
 
-
-`/usr/bin/curl --compressed -s https://api.elrond.com/node/heartbeatstatus > heartbeatstatus.json`
-`/usr/bin/curl --compressed -s https://api.elrond.com/validator/statistics > statistics.json`
-
-heartbeatstatus_file = File.read("heartbeatstatus.json")
-statistics_file = File.read("statistics.json")
-
-if ! valid_json?(heartbeatstatus_file)
-	puts "JSON file is not valid"
-	exit
+def extract_obs_status(status_hash,network)
+    metricLabels="displayName=\"#{status_hash['erd_node_display_name']}\",nodeType=\"#{status_hash['erd_node_type']}\",network=\"#{network}\",shardID=\"#{status_hash['erd_shard_id']}\",validatorPubkey=\"#{status_hash['erd_p ublic_key_block_sign']}\",syncStatus=\"#{status_hash['erd_is_syncing']}\""
+    puts "elrond_obs_sync_status{#{metricLabels}} #{status_hash['erd_is_syncing']}"
+    puts "elrond_obs_epochNumber{#{metricLabels}} #{status_hash['erd_epoch_number']}"
 end
 
-if ! valid_json?(statistics_file)
-	puts "JSON file is not valid"
-	exit
+
+# The function will read data from API and write response to filename passed as the second argument
+def  read_heartbeatstatus(api_url, fname)
+  `/usr/bin/curl --compressed -s #{api_url}/node/heartbeatstatus > #{fname}`
 end
 
-heartbeatstatus = JSON.parse(heartbeatstatus_file)
-statistics = JSON.parse(statistics_file)
+# The function will read data from API and write response to filename passed as the second argument
+def  read_statistics(api_url, fname)
+  `/usr/bin/curl --compressed -s #{api_url}/validator/statistics > #{fname}`
+end
 
-heartbeats_array = heartbeatstatus['data']['heartbeats']
-statistics_hash = statistics['data']['statistics']
+def read_observer_status(api_url, fname)
+  `/usr/bin/curl --compressed -s #{api_url}/node/status > #{fname}`
+end
 
-extract_info(heartbeats_array, statistics_hash,"elrondcom")
+def validate_api_response(json_data)
+  if ! valid_json?(json_data)
+  	puts "JSON file is not valid"
+  	exit
+  end
+end
+
+# Read the Observer stats only if the API allows it. If the variable is empty it will not generate those metrics
+def get_metrics(api_url, network, heartbeat_fname, statistics_fname, obstats_fname="")
+  read_heartbeatstatus(api_url, heartbeat_fname)
+  read_statistics(api_url, statistics_fname)
+
+  heartbeatstatus_file = File.read(heartbeat_fname)
+  statistics_file = File.read(statistics_fname)
+
+  validate_api_response(heartbeatstatus_file)
+  validate_api_response(statistics_file)
+
+  heartbeatstatus = JSON.parse(heartbeatstatus_file)
+  statistics = JSON.parse(statistics_file)
+
+  heartbeats_array = heartbeatstatus['data']['heartbeats']
+  statistics_hash = statistics['data']['statistics']
+
+  extract_info(heartbeats_array, statistics_hash)
+
+  unless obstats_fname.to_s.strip.empty?
+    read_observer_status(api_url, obstats_fname)
+    status_file = File.read(obstats_fname)
+    validate_api_response(status_file)
+    status = JSON.parse(status_file)
+    status_hash = status['data']['metrics']
+    extract_obs_status(status_hash, network)
+  end
+
+end
+
+api_url = "https://api.elrond.com"
+network = "mainnet"
+get_metrics(api_url, network, "hbeat-mainnet.json", "statistics-mainnet.json")
+
+api_url = "https://testnet-api.elrond.com"
+network = "testnet"
+get_metrics(api_url, network, "hbeat-testnet.json", "statistics-testnet.json")
